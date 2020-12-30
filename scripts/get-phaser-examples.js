f2e4df8c79b1cfac78daad52d67be490240fc9fa
PhaserEditor2D <developers@phasereditor2d.com>
2020-12-30 16:13:50 -0500

    Get phaser examples code.

--- scripts/get-phaser-examples.js
+++ scripts/get-phaser-examples.js
@@ -48,8 +48,65 @@ function resizeImage(dir) {
     }
 }
 
-resizeImage(path.join(plugin, "phaser3-examples/screenshots"));
+function processScreenshots() {
 
-child_process.execSync("mkdir -p " + path.join(plugin, "phaser3-examples/atlas/"));
-child_process.execSync("npm run build-examples-atlas");
-child_process.execSync("rm -Rf " + path.join(plugin, "phaser3-examples/screenshots"));
+    resizeImage(path.join(plugin, "phaser3-examples/screenshots"));
+
+    child_process.execSync("mkdir -p " + path.join(plugin, "examples-screenshots-atlas/"));
+    child_process.execSync("npm run build-examples-atlas");
+    child_process.execSync("rm -Rf " + path.join(plugin, "phaser3-examples/"));
+}
+
+function processCode() {
+
+    const data = {};
+
+    const root = path.join(phaserHome, "phaser3-examples/public/src");
+
+    function processCode2(dir) {
+
+        const files = fs.readdirSync(dir);
+
+        for (const file of files) {
+
+            if (file === "3.24") {
+
+                continue;
+            }
+
+            if (file.startsWith(".")) {
+
+                continue;
+            }
+
+            const fullName = path.join(dir, file);
+
+            if (fullName.endsWith(".js")) {
+
+                const relName = path.relative(root, fullName);
+
+                console.log("processing " + relName);
+
+                const content = fs.readFileSync(fullName);
+
+                data[relName] = content.toString();
+
+            } else {
+
+                if (fs.statSync(fullName).isDirectory()) {
+
+                    processCode2(fullName);
+                }
+            }
+        }
+    }
+
+    processCode2(root);
+
+    fs.writeFileSync(path.join(plugin, "phaser-examples-code.json"), JSON.stringify(data, null, 2));
+}
+
+processScreenshots();
+
+processCode();
+
