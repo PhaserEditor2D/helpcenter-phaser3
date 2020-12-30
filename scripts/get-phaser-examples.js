94047f5e7877b250a2581e18dd819cfaf4e24745
PhaserEditor2D <developers@phasereditor2d.com>
2020-12-30 15:34:02 -0500

    Build examples screenshot atlas.

---
+++ scripts/get-phaser-examples.js
@@ -0,0 +1,55 @@
+#!/usr/bin/node
+const child_process = require("child_process");
+const process = require("process");
+const path = require("path");
+const fs = require("fs");
+
+const phaserHome = process.env["PHASER_PATH"];
+const plugin = "../source/app/plugins/helpcenter.phaser/data";
+/*
+child_process.execSync(`rm -Rf ${path.join(plugin, "phaser3-examples/")}`)
+child_process.execSync(`mkdir -p ${path.join(plugin, "phaser3-examples/")}`)
+
+child_process.execSync("cp -R "
+    + path.join(phaserHome, "phaser3-examples/public/screenshots/") + " "
+    + path.join(plugin, "phaser3-examples/"));
+
+function resizeImage(dir) {
+
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+
+        if (file.startsWith(".")) {
+
+            continue;
+        }
+
+        const fullName = path.join(dir, file);
+
+
+        if (fullName.endsWith(".png")) {
+
+            console.log("processing " + fullName);
+
+            const outputName = path.join(path.dirname(fullName), file.substring(0, file.length - 4) + ".jpg");
+
+            child_process.execSync(`ffmpeg -loglevel panic -i "${fullName}" -vf scale=128:96 "${outputName}"`);
+
+            child_process.execSync(`rm "${fullName}"`);
+
+        } else {
+
+            if (fs.statSync(fullName).isDirectory()) {
+
+                resizeImage(fullName);
+            }
+        }
+    }
+}
+
+resizeImage(path.join(plugin, "phaser3-examples/screenshots"));
+*/
+child_process.execSync("mkdir -p " + path.join(plugin, "phaser3-examples/atlas/"));
+child_process.execSync("npm run build-examples-atlas");
+child_process.execSync("rm -Rf " + path.join(plugin, "phaser3-examples/screenshots"));
