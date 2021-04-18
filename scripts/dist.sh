f90dd911ed7d442b60a6037a80862bbf6efd8051
PhaserEditor2D <developers@phasereditor2d.com>
2021-04-18 05:43:42 -0400

    Build script.

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -9,6 +9,8 @@ mkdir dist/
 #npm run ts-clean
 #npm run ts-build
 
-cp -R source/app dist/
+cp -R source/* dist/
 
 rm -R dist/app/node_modules dist/app/*.sh dist/app/*.json
+rm -R dist/app/plugins/**/src/
+rm -R dist/app/plugins/**/_out/*.ts*
