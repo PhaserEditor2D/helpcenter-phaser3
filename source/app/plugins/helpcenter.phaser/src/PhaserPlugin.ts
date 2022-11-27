6a7a81a9c445884fbbe38dcb40168410e1aa9e9c
PhaserEditor2D <developers@phasereditor2d.com>
2022-11-27 09:11:11 -0500

    Updates to Phaser 3.55.2.

--- source/app/plugins/helpcenter.phaser/src/PhaserPlugin.ts
+++ source/app/plugins/helpcenter.phaser/src/PhaserPlugin.ts
@@ -1,6 +1,6 @@
 namespace helpcenter.phaser {
 
-    export const PHASER_VER = "3.55.0";
+    export const PHASER_VER = "3.55.2";
 
     export const DEFAULT_PHASER_LABS_URL = "https://labs.phaser.io";
 
